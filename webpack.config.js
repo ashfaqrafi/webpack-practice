7d920cc0cc1cf495a0977ac324641968748b190c
Ashfaq Mahmood <ashfaqmahmood@Ashfaq-MacBook-Pro.local>
2016-08-21 16:06:21 +0600

    second webpack practice

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+'use strict';
+var path = require('path');
+var webpack = require('webpack');
+var ngAnnotatePlugin = require('ng-annotate-webpack-plugin');
+
+module.exports = {
+  context: path.join(__dirname),
+  entry: {
+    'time-logger': './time-logger-app.js',
+    'time-logger-2': './time-logger-app-2.js'
+  },
+  output: {
+    path: path.join(__dirname),
+    filename: '[name].js'
+  },
+  devtool: 'source-map',
+  module: {
+    preLoaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'jshint-loader' }
+    ],
+    loaders: [
+      {test: /\.js$/, loader: 'ng-annotate'},
+      {test: /\.css$/, loaders: ['style', 'css']},
+      {test: /\.html$/, loader: 'raw'}
+    ]
+  },
+  plugins: [
+    new ngAnnotatePlugin({add : true}),
+    new webpack.optimize.CommonsChunkPlugin('common.js')
+  ],
+  externals: {
+    angular: true,
+    'angular-route': '"ngRoute"',
+    'moment': true
+  }
+};
